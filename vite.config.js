e6a9879dcf36933866e629f2178642d986362b10
Leviosa.id <macbook@192.168.98.181>
2026-09-06 08:29:15 +0700

    Edit

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
       manifest: {
         name: 'Dapur MUA',
         short_name: 'Dapur MUA',
