51678b8d84fc437f242e1285aaf0195121aa6651
Leviosa.id <macbook@192.168.5.106>
2026-09-07 21:11:46 +0700

    Edit

--- src/pages/Keuangan.jsx
+++ src/pages/Keuangan.jsx
@@ -166,7 +166,7 @@ export default function Keuangan() {
       { header: 'Bulan', key: 'bulan', width: 12 },
       { header: 'Booking', key: 'booking', width: 10 },
       { header: 'Klien', key: 'klien', width: 8 },
-      { header: 'Pembayaran Klien', key: 'belanja', width: 15 },
+      { header: 'Pembayaran', key: 'belanja', width: 15 },
       { header: 'Transport', key: 'transport', width: 13 },
       { header: 'Omzet', key: 'omzet', width: 15 },
       { header: 'Komisi Tim', key: 'komisi', width: 13 },
@@ -274,7 +274,7 @@ export default function Keuangan() {
             <div className="bar-grid">
               <div className="card-keuangan">
                 <div className="card-head-keuangan">
-                  <h3>Total Pembayaran Klien {filterTahun}</h3>
+                  <h3>Total Pembayaran{filterTahun}</h3>
                   <span className="chart-total-belanja">{formatRupiah(totalTahun.belanja)}</span>
                 </div>
                 <MonthlyBarChart
@@ -333,7 +333,7 @@ export default function Keuangan() {
                     <th>Bulan</th>
                     <th className="right">Booking</th>
                     <th className="right">Klien</th>
-                    <th className="right">Pembayaran Klien</th>
+                    <th className="right">Pembayaran</th>
                     <th className="right">Transport</th>
                     <th className="right">Omzet</th>
                     <th className="right">Komisi Tim</th>
